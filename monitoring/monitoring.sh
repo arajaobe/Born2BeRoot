aca6dc18476c2cffe0cadc09ccaa1c293a6c7754
arajaobe <andylraj@gmail.com>
2026-08-13 13:15:31 +0300

    fix root

--- monitoring/monitoring.sh
+++ monitoring/monitoring.sh
@@ -5,14 +5,18 @@ ARCH="$(uname -a)"
 PHYS_CPU="$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)"
 VCPU="$(grep "processor" /proc/cpuinfo | wc -l)"
 MEM_PCT="$(free -m | grep "Mem:" | awk '{printf("%d/%dMB (%.2f%%)", $3, $2, $3/$2*100)}')"
-DISK="$(df -BM / | awk 'NR==2 {printf("%d/%dMB (%d%%)", $3, $2, $5)}')"
+DISK="$(df -m | grep '^/dev/' | awk '{use += $3; total += $2} END {printf("%.1f/%.1fGb (%d%%)", use/1024, total/1024, use/total*100)}')"
 CPU_LOAD="$(vmstat 1 2 | tail -1 | awk '{printf("%.1f%%", 100-$15)}')"
 LAST_BOOT="$(who -b | awk '{print $3 " " $4}')"
 LVM_checker="$(lsblk | grep -q "lvm" && echo "yes" || echo "no")"
 TCP_CONN="$(ss -ta | grep -c "ESTAB")"
 USER_COUNT="$(who | awk '{print $1}' | sort -u | wc -l)"
-IP_MAC="$(hostname -I | awk '{print $1}') ($(ip link | awk '/link\/ether/ {print $2}'))"
-SUDO_COUNT="$(journalctl -q _COMM=sudo --no-pager | grep -c COMMAND)"
+IP_MAC="$(hostname -I | awk '{print $1}') ($(ip link | awk '/link\/ether/ {print $2}' | head -n 1))"
+if [ -f /var/log/sudo/sudo.log ]; then
+    SUDO_COUNT="$(grep -c 'COMMAND' /var/log/sudo/sudo.log)"
+else
+    SUDO_COUNT="$(journalctl -q _COMM=sudo --no-pager | grep -c COMMAND)"
+fi
 
 # Output
 echo "#Architecture: $ARCH"
